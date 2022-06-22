7ddb905157952dfdd47219fd88132e37f0c836c2
CloudAndMist <854947489@qq.com>
2022-06-22 15:02:38 +0800

    新增数据表字段最大长度统计

---
+++ pycode/data_preProcess/novel_attr_len_analysis.py
@@ -0,0 +1,21 @@
+import json
+import codecs
+import os
+
+if __name__ == '__main__':
+    with open("../data/novel_3.json", encoding="utf-8") as file:
+        file_json = json.load(file)
+    max_title = 1
+    max_url = 1
+    max_cover = 1
+    max_introduce = 1
+    for line in file_json:
+        max_title = max([len(line['title']), max_title])
+        max_url = max([len(line['url']), max_url])
+        max_cover = max([len(line['cover']), max_cover])
+        max_introduce = max([len(line['introduce']), max_introduce])
+    print("max_title: ", max_title)
+    print("max_url: ", max_url)
+    print("max_cover: ", max_cover)
+    print("max_introduce: ", max_introduce)
+
