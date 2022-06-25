b815f5a9926251d368fa9b123ffa445b83196071
Yizhang-Zhu <derrickzhuyz@outlook.com>
2022-06-25 15:09:49 +0800

    userXxxHistoryDao的getXxxByUserId和getXxxAll方法

--- back/dao/userAnimeHistoryDao.py
+++ back/dao/userAnimeHistoryDao.py
@@ -6,7 +6,46 @@
 @Description:
 '''
 
-'''
-get by uid
-'''
+import pymysql
 
+'''
+根据用户id获取其所有的动漫阅览记录
+返回格式：
+[{'ahid': 1, 'uid': 10000, 'aid': 11, 'score': 1.0, 'ratio': 0.32, 'thumb': 1, 'collect': 0, 'time': xxx}, {..}]
+'''
+def getAnimeHistoryByUserId(userId: int):
+    db = pymysql.connect(host="localhost", user="root", password="xxxx", port=3307, database='ars')
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    sql = """SELECT * FROM useranimehistory WHERE uid = %d"""%(userId)
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        db.commit()
+        db.close()
+        return result
+    except:
+        db.rollback()
+        return {'message': 'get anime fail'}
+
+# print(getAnimeHistoryByUserId(10001))
+
+
+'''
+返回useranimehistroy中全部用户阅览数据
+数据量大，谨慎操作！
+'''
+def getAnimeHistoryAll():
+    db = pymysql.connect(host="localhost", user="root", password="xxxx", port=3307, database='ars')
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    sql = """SELECT * FROM useranimehistory"""
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        db.commit()
+        db.close()
+        return result
+    except:
+        db.rollback()
+        return {'message': 'get anime fail'}
+
+# print(getAnimeHistoryAll())
--- back/dao/userNovelHistoryDao.py
+++ back/dao/userNovelHistoryDao.py
@@ -2,6 +2,50 @@
 # -*- coding: utf-8 -*-
 '''
 @Time: 2022/6/24 9:27
-@Author: Jocx
+@Author: Jocx, derrick
 @Description:
-''' 
+'''
+
+import pymysql
+
+'''
+根据用户id获取其所有的动漫阅览记录
+返回格式：
+[{'nhid': 1, 'uid': 10000, 'nid': 11, 'score': 1.0, 'ratio': 0.32, 'thumb': 1, 'collect': 0, 'time': xxx}, {..}]
+'''
+def getNovelHistoryByUserId(userId: int):
+    db = pymysql.connect(host="localhost", user="root", password="xxxx", port=3307, database='ars')
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    sql = """SELECT * FROM usernovelhistory WHERE uid = %d"""%(userId)
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        db.commit()
+        db.close()
+        return result
+    except:
+        db.rollback()
+        return {'message': 'get novel fail'}
+
+# print(getNovelHistoryByUserId(10001))
+
+
+'''
+返回usernovelhistroy中全部用户阅览数据
+数据量大，谨慎操作！
+'''
+def getNovelHistoryAll():
+    db = pymysql.connect(host="localhost", user="root", password="xxxx", port=3307, database='ars')
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+    sql = """SELECT * FROM usernovelhistory"""
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        db.commit()
+        db.close()
+        return result
+    except:
+        db.rollback()
+        return {'message': 'get novel fail'}
+
+# print(getNovelHistoryAll())
