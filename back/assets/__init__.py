f5d8c1b431994d841e46eca3ecc54f8d4b4a0b02
CloudAndMist <854947489@qq.com>
2022-06-27 11:34:38 +0800

    新增assets

---
+++ back/assets/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+@Time: 2022/6/27 11:25
+@Author: CloudAndMist
+@Description:
+'''
--- back/service/userAnalysisService.py
+++ back/service/userAnalysisService.py
@@ -6,8 +6,31 @@
 @Description:
 '''
 
+import json
+from dao.userAnimeHistoryDao import getAnimeHistoryByUserId
+from dao.userComicHistoryDao import getComicHistoryByUserId
+from dao.userNovelHistoryDao import getNovelHistoryByUserId
+
+def analysisWatchRatio(ratio: float):
+    if ratio > 0.7:
+        return ratio
+    elif ratio > 0.4:
+        return ratio * 0.75
+    return ratio * 0.5
+
 
 def userAnalysis(uid: int):
+    userAnimeHistoryList = getAnimeHistoryByUserId(uid)
+    userComicHistoryList = getComicHistoryByUserId(uid)
+    userNovelHistoryList = getNovelHistoryByUserId(uid)
+    if (userAnimeHistoryList is None) or (userComicHistoryList is None) or (userNovelHistoryList is None):
+        raise
+    userImageDataDir = {}
+    with open("../assets/userImageDataList/feizhaiSelectList.json", encoding="utf-8") as file:
+        userImageDataDir['feizhai'] = json.load(file)
+    print(type(userImageDataDir['feizhai']))
+
+
     result = {}
 
     return result
